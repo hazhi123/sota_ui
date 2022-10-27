df6fcbfeadd073281908f79ad1c34e02c81b76a0
Yordano Hernandez <hazhi123@gmail.com>
2022-10-27 15:57:06 -0400

    se agrega validemail extension

--- lib/utils/other_util.dart
+++ lib/utils/other_util.dart
@@ -61,6 +61,14 @@ extension StringCasingExtension on String {
       .join(' ');
 }
 
+extension EmailValidator on String {
+  bool isValidEmail() {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(this);
+  }
+}
+
 timerSui(time, funcion) {
   Timer(
       time.toString().length > 2
